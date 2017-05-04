22710c695da318a5061c9776c39bee1073008c08
Gustavo Amezcua <amezcuakennedy@gmail.com>
2017-05-04 09:26:09 -0700

    first

---
+++ inc/footer.php
@@ -0,0 +1,21 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="https://twitter.com/GustavoAmezcua8">Twitter</a></li>
+				<li><a href="https://www.facebook.com/gus.kennedy.31">Facebook</a></li>
+			</ul>
+<?php 
+date_default_timezone_set("America/Los_Angeles");
+?>
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
